ddbd9cd820d6798192dc0104e5b998d9e57f3446
FrankTimmons <franktimmonspdx@gmail.com>
2022-09-30 15:55:41 -0700

    Update a list of players for a room when someone joins

--- My project/Assets/Scripts/Launcher.cs
+++ My project/Assets/Scripts/Launcher.cs
@@ -20,9 +20,10 @@ public class Launcher : MonoBehaviourPunCallbacks
 
   public GameObject createRoomScreen;
   public TMP_InputField roomNameInput;
+  private List<TMP_Text> allPlayerNames = new List<TMP_Text>();
 
   public GameObject roomScreen;
-  public TMP_Text roomNameText;
+  public TMP_Text roomNameText, playerNameLabel;
 
   public GameObject errorScreen;
   public TMP_Text errorText;
@@ -92,6 +93,30 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     roomScreen.SetActive(true);
     roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+    PhotonNetwork.NickName = Random.Range(0, 1000).ToString();
+
+    ListAllPlayers();
+  }
+
+  private void ListAllPlayers()
+  {
+    foreach(TMP_Text player in allPlayerNames)
+    {
+      Destroy(player.gameObject);
+    }
+    allPlayerNames.Clear();
+
+    Player[] players = PhotonNetwork.PlayerList;
+
+    for(int i = 0; i < players.Length; i++)
+    {
+      TMP_Text newPlayerLabel = Instantiate(playerNameLabel, playerNameLabel.transform.parent);
+      newPlayerLabel.text = players[i].NickName;
+      newPlayerLabel.gameObject.SetActive(true);
+
+      allPlayerNames.Add(newPlayerLabel);
+    }
   }
 
   public override void OnCreateRoomFailed(short returnCode, string message)
