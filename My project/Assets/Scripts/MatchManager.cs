83d3f07677df588b09ecf16bfd06268d5fa6bd02
FrankTimmons <franktimmonspdx@gmail.com>
2022-10-04 18:09:03 -0700

    Fix the lighting when testing the game

--- My project/Assets/Scripts/MatchManager.cs
+++ My project/Assets/Scripts/MatchManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 public class MatchManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public static MatchManager instance;
 
-    // Update is called once per frame
-    void Update()
+  private void Awake()
+  {
+    instance = this;
+  }
+
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (!PhotonNetwork.IsConnected)
     {
-        
+      SceneManager.LoadScene(0);
     }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
 }
