de8d5d66a89f035fd45a59948707921d24a020ba
FrankTimmons <franktimmonspdx@gmail.com>
2022-10-07 13:12:38 -0700

    Have the selected guns change for every player on the network

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -59,7 +59,8 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
     UIController.instance.weaponTempSlider.maxValue = maxHeat;
 
-    SwitchGun();
+    //SwitchGun();
+    photonView.RPC("SetGun", RpcTarget.All, selectedGun);
 
     currentHealth = maxHealth;
 
@@ -193,7 +194,8 @@ public class PlayerController : MonoBehaviourPunCallbacks
         {
           selectedGun = 0;
         }
-        SwitchGun();
+        //SwitchGun();
+        photonView.RPC("SetGun", RpcTarget.All, selectedGun);
       }
       else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0)
       {
@@ -202,7 +204,8 @@ public class PlayerController : MonoBehaviourPunCallbacks
         {
           selectedGun = allGuns.Length - 1;
         }
-        SwitchGun();
+        //SwitchGun();
+        photonView.RPC("SetGun", RpcTarget.All, selectedGun);
       }
 
       for (int i = 0; i < allGuns.Length; i++)
@@ -210,7 +213,8 @@ public class PlayerController : MonoBehaviourPunCallbacks
         if (Input.GetKeyDown((i + 1).ToString()))
         {
           selectedGun = i;
-          SwitchGun();
+          //SwitchGun();
+          photonView.RPC("SetGun", RpcTarget.All, selectedGun);
         }
       }
 
@@ -315,5 +319,15 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
     allGuns[selectedGun].gameObject.SetActive(true);
     allGuns[selectedGun].muzzleFlash.SetActive(false);
+  }
+
+  [PunRPC]
+  public void SetGun(int gunToSwitchTo)
+  {
+    if(gunToSwitchTo < allGuns.Length)
+    {
+      selectedGun = gunToSwitchTo;
+      SwitchGun();
+    }
   }
 }
